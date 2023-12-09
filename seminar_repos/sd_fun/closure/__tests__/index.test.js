d2c45d15623dca4068216362e07495fc77716511
nc-danika-crawley <danika.crawley@northcoders.com>
2023-12-09 11:30:00 +0000

    BACKUP_2023-12-09

--- seminar_repos/sd_fun/closure/__tests__/index.test.js
+++ seminar_repos/sd_fun/closure/__tests__/index.test.js
@@ -1,1 +1,31 @@
 const shoppingList = require('../index');
+
+describe('shoppingList', () => {
+	test('shoppingList should return a function', () => {
+		expect(typeof shoppingList()).toBe('function');
+	});
+	test('returned function returns shopping list when invoked', () => {
+		const testList = shoppingList('potatoes');
+
+		const actual = testList();
+		const expected = ['potatoes'];
+
+		expect(actual).toEqual(expected);
+	});
+	test('returned function - when invoked with an item returns updated list', () => {
+		const testList = shoppingList('potatoes');
+
+		const actual = testList('water');
+		const expected = ['potatoes', 'water'];
+
+		expect(actual).toEqual(expected);
+	});
+	test('shoppingList should take any number of arguments', () => {
+		const testList = shoppingList('potatoes', 'carrots', 'sprouts');
+
+		const actual = testList();
+		const expected = ['potatoes', 'carrots', 'sprouts'];
+
+		expect(actual).toEqual(expected);
+	});
+});
--- seminar_repos/sd_fun/closure/index.js
+++ seminar_repos/sd_fun/closure/index.js
@@ -1,3 +1,15 @@
-function shoppingList() {}
+function shoppingList(...items) {
+	const newList = [...items];
+
+	function innerFunc(newItem) {
+		// add new item to shopping list
+		if (newItem) {
+			newList.push(newItem);
+		}
+
+		return newList;
+	}
+	return innerFunc;
+}
 
 module.exports = shoppingList;
