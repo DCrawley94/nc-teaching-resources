db585590b1ed91ed6cef714514b9cc68da06301a
nc-danika-crawley <danika.crawley@northcoders.com>
2024-04-13 11:30:00 +0100

    BACKUP_2024-04-13

--- seminar_repos/de_be/be_integration_testing/server.py
+++ seminar_repos/de_be/be_integration_testing/server.py
@@ -2,13 +2,21 @@ from fastapi import FastAPI, HTTPException
 from db.connection import connect_to_db
 from server_utils import format_response
 from pg8000.exceptions import DatabaseError
+from pydantic import BaseModel
+
+
+class NewGame(BaseModel):
+    game_title: str
+    release_year: int
+    console_name: str
+    image_url: str
 
 
 app = FastAPI()
 
 
 @app.get('/api/reviews/{review_id}')
-def get_review_by_id(review_id):
+def get_review_by_id(review_id:int):
     conn = None
     try:
         conn = connect_to_db()
@@ -31,6 +39,8 @@ def get_review_by_id(review_id):
 
         return response
     except DatabaseError as db_error:
+        # This error handling is only required if you're not relying on 
+        #   pydantics 422s
         error = db_error.args[0]
         error_code = error['C']
         error_message = error['M']
@@ -39,6 +49,25 @@ def get_review_by_id(review_id):
                 status_code=400, 
                 detail=f"review_id should be an integer - {error_message}"
             )
+    finally:
+        if conn:
+            conn.close()
+
+@app.post('/api/games', status_code=201)
+def post_game(new_game: NewGame):
+    conn = None
+    try:
+        conn = connect_to_db()
+        new_game = conn.run("""
+        INSERT INTO games
+        (game_title, release_year, console_name, image_url)
+        VALUES
+        (:game_title, :release_year, :console_name, :image_url)
+        RETURNING *;
+        """, **dict(new_game))
+        columns = [col['name'] for col in conn.columns]
+        response = format_response(columns, new_game, "game")
+        return response
     finally:
         if conn:
             conn.close()
--- seminar_repos/de_be/be_integration_testing/test/test_server.py
+++ seminar_repos/de_be/be_integration_testing/test/test_server.py
@@ -1,5 +1,12 @@
 from fastapi.testclient import TestClient
 from server import app
+from db.data.index import data
+from db.seed import seed
+import pytest
+
+@pytest.fixture(autouse=True)
+def reset_db():
+    seed(**data)
 
 
 class TestGetReviewByID:
@@ -38,15 +45,58 @@ class TestGetReviewByID:
         assert response.status_code == 404
         assert response.json() == expected
 
-    # def test_422_malformed_review_id(self):
-    #     client = TestClient(app)
-    #     response = client.get("/api/reviews/not_an_id")
+    # You can do one of these two tests depending on how you want 
+    #   to handle malformed IDs
 
-    #     assert response.status_code == 422
+    def test_422_malformed_review_id(self):
+        """ Test for if you want to let pydantic do the work for you """
+        client = TestClient(app)
+        response = client.get("/api/reviews/not_an_id")
+
+        assert response.status_code == 422
 
     def test_400_malformed_review_id(self):
+        """ Test for if you want to respond with a 400 """
         client = TestClient(app)
         response = client.get("/api/reviews/not_an_id")
 
         assert response.status_code == 400
-        assert response.json() == 'yeet'
+        assert response.json() == (
+            'review_id should be an integer'
+        )
+
+
+class TestPostGame:
+    def test_201_game_returned(self):
+        client = TestClient(app)
+        new_game = {
+            "game_title": "Skyrim 2",
+            "release_year": 2024,
+            "console_name": "Playstation 2",
+            "image_url": "test_url"
+        }
+        expected_response_body = {
+            'game': {
+                'game_id': 4,
+                'game_title': 'Skyrim 2',
+                'release_year': 2024,
+                'console_name': 'Playstation 2',
+                'image_url': 'test_url'
+            }
+        }
+        response = client.post('/api/games', json=new_game)
+        assert response.status_code == 201
+        assert response.json() == expected_response_body
+
+    def test_422_malformed_game(self):
+        """ 
+        Could also treat this as a 400 like the previous endpoint if you wish
+        """
+        client = TestClient(app)
+        new_game = {
+            "game_title": "Skyrim 2",
+            "console_name": "Playstation 2",
+            "image_url": "test_url"
+        }
+        response = client.post('/api/games', json=new_game)
+        assert response.status_code == 422
