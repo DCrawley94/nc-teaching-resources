97c9f254d775a6ef699fe0eb1883ed2b394a7f9f
nc-danika-crawley <danika.crawley@northcoders.com>
2024-04-18 11:30:00 +0100

    BACKUP_2024-04-18

---
+++ seminar_repos/de_cloud/make_intro/src/example.py
@@ -0,0 +1,23 @@
+"""
+# count_ice_cream
+
+- create a function that accepts a list as an argument 
+- this list will either contain strings or lists which can also contain strings 
+- The function should count the number of times a string of "ice cream" 
+    appears inside these lists.
+"""
+
+
+def count_ice_creams(food_list):
+    count = 0
+
+    for el in food_list:
+        if el == 'ice cream':
+            count += 1
+        elif isinstance(el, list):
+            count += count_ice_creams(el)
+
+    return count
+
+
+# count_ice_creams([["ice cream"], ["ice cream"]])
