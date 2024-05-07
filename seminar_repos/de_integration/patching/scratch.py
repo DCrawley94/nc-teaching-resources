698c0936b592217336a5905d949a72632cbf6452
nc-danika-crawley <danika.crawley@northcoders.com>
2024-05-07 11:30:00 +0100

    BACKUP_2024-05-07

--- seminar_repos/de_integration/patching/scratch.py
+++ seminar_repos/de_integration/patching/scratch.py
@@ -21,20 +21,20 @@ conn = Connection(
         database='nc_games'
     )
 
-print(Connection, ' <<< class')
-print(conn, ' <<< instance')
-print(Connection.run, ' <<< class method')
-print(conn.run, ' <<< instance method')
-print(Connection.run is conn.run, ' <<< class method is instance method? 😱')
+# print(Connection, ' <<< class')
+# print(conn, ' <<< instance')
+# print(Connection.run, ' <<< class method')
+# print(conn.run, ' <<< instance method')
+# print(Connection.run is conn.run, ' <<< class method is instance method? 😱')
 
-# with patch("pg8000.native.Connection.run", return_value=[[1,2,3]]) as mc:
-#     conn = Connection(
-#         host='localhost',
-#         user='danika',
-#         database='nc_games'
-#     )
-#     print(Connection, ' <<< class')
-#     print(conn, ' <<< instance')
-#     print(Connection.run, ' <<< class method')
-#     print(conn.run, ' <<< instance method')
-#     print(Connection.run is conn.run, ' <<< class method is instance method? 😱')
+with patch("__main__.conn.run", return_value=[[1,2,3]]) as mc:
+    # conn = Connection(
+    #     host='localhost',
+    #     user='danika',
+    #     database='nc_games'
+    # )
+    print(Connection, ' <<< class')
+    print(conn, ' <<< instance')
+    print(Connection.run, ' <<< class method')
+    print(conn.run, ' <<< instance method')
+    print(Connection.run is conn.run, ' <<< class method is instance method? 😱')
