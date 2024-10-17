610848478e7be930be0615d9df46bd94c93937cb
nc-danika-crawley <danika.crawley@northcoders.com>
2024-10-17 15:51:11 +0100

    manual backup

--- seminar_repos/03_data_fun/09_terraform_tasks/01_moving_from_console_to_terraform/01_rds/rds.tf
+++ seminar_repos/03_data_fun/09_terraform_tasks/01_moving_from_console_to_terraform/01_rds/rds.tf
@@ -1,10 +1,10 @@
-resource "aws_db_instance" "rds_example" {
-  allocated_storage   = 10
-  engine              = "postgresql"
+resource "aws_db_instance" "default" {
+  allocated_storage   = 20
+  engine              = "postgres"
   engine_version      = "14.10"
-  instance_class      = "db.t3.micro"
+  instance_class      = "db.t4g.micro"
   username            = "postgres"
   password            = "password123"
-  identifier          = "rds-example"
+  identifier          = "example-database"
   skip_final_snapshot = true
 }
--- seminar_repos/03_data_fun/09_terraform_tasks/01_moving_from_console_to_terraform/02_ec2/ec2.tf
+++ seminar_repos/03_data_fun/09_terraform_tasks/01_moving_from_console_to_terraform/02_ec2/ec2.tf
@@ -1,1 +1,20 @@
+resource "aws_instance" "example-ec2" {
+  ami             = "ami-0acc77abdfc7ed5a6"
+  instance_type   = "t2.micro"
+  key_name        = "dc-ssh"
+  security_groups = [aws_security_group.ec2-example-sg.name]
+}
 
+resource "aws_security_group" "ec2-example-sg" {
+  name        = "example"
+  description = "allow ssh access into the instance"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2-ssh" {
+  security_group_id = aws_security_group.ec2-example-sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  to_port     = 22
+  ip_protocol = "tcp"
+}
