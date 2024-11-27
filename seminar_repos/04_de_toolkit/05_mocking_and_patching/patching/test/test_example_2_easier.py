3f8c23c81d225845d58bb446bbb7012a1465bf3e
nc-danika-crawley <danika.crawley@northcoders.com>
2024-11-27 11:30:01 +0000

    BACKUP_2024-11-27

--- seminar_repos/04_de_toolkit/05_mocking_and_patching/patching/test/test_example_2_easier.py
+++ seminar_repos/04_de_toolkit/05_mocking_and_patching/patching/test/test_example_2_easier.py
@@ -16,6 +16,7 @@ class MockPoolManager:
         return response
 
 
+# Could replace PoolManager with a mock class
 def test_get_request_200():
     with patch("src.example_2_easier.PoolManager", return_value=MockPoolManager(200)):
         assert check_api_status("www.test.com") == {
@@ -25,13 +26,37 @@ def test_get_request_200():
         }
 
 
+# Or just directly add the method and it's return value
 def test_get_request_not_200():
-    pass
+    with patch("src.example_2_easier.PoolManager") as mock_pool_manager:
+        mock_pool_manager().request.return_value = Mock(status=404)
+        assert check_api_status("www.test.com") == {
+            "status": "failure",
+            "code": 404,
+            "message": "API returned an error",
+        }
 
 
-def test_http_error():
-    pass
+# Cheeky patch decorator with mocking arrangement steps broken down
+@patch("src.example_2_easier.PoolManager")
+def test_http_error(mock_pool_manager):
+    mock_request = Mock(side_effect=HTTPError("Some HTTP error"))
+    mock_pool_manager.return_value = Mock(request=mock_request)
+
+    assert check_api_status("www.test.com") == {
+        "status": "error",
+        "code": None,
+        "message": "HTTP error occurred: Some HTTP error",
+    }
 
 
-def test_generic_exception():
-    pass
+# Decorator with the Mock set up in one line
+@patch("src.example_2_easier.PoolManager")
+def test_generic_exception(mock_pool_manager):
+    mock_pool_manager().request.side_effect = Exception("Generic Exception")
+
+    assert check_api_status("www.test.com") == {
+        "status": "error",
+        "code": None,
+        "message": "An error occurred: Generic Exception",
+    }
