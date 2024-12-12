56d5ecb0c7dec4d8374521c4bc24e80e99b66d62
nc-danika-crawley <danika.crawley@northcoders.com>
2024-12-12 11:30:02 +0000

    BACKUP_2024-12-12

--- seminar_repos/02_python_fun/03_testing_hofs/02_dep_injection/src/process_numbers.py
+++ seminar_repos/02_python_fun/03_testing_hofs/02_dep_injection/src/process_numbers.py
@@ -1,6 +1,3 @@
-from src.utils import filter_odd_numbers, double_numbers
-
-
 def process_numbers(numbers, filter_func, transform_func=None):
     """
     This function processes a given list of numbers, first by filtering the
@@ -12,13 +9,4 @@ def process_numbers(numbers, filter_func, transform_func=None):
         filter_func: function for filtering a list of numbers
         *OPTIONAL* transform_func: function for transforming a list of numbers
     """
-    processed_nums = filter_func(numbers)
-
-    if transform_func:
-        processed_nums = transform_func(processed_nums)
-
-    return sum(processed_nums)
-
-
-if __name__ == "__main__":
-    print(process_numbers([1, 2, 3, 4, 5], filter_odd_numbers, double_numbers))
+    pass
