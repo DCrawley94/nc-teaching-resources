1cac6fa3e09b3de86ba0c260089021357ff5ee6c
nc-danika-crawley <danika.crawley@northcoders.com>
2024-12-19 11:30:01 +0000

    BACKUP_2024-12-19

--- seminar_repos/02_python_fun/06_data_structures/03_solution/queue_solution/src/queue.py
+++ seminar_repos/02_python_fun/06_data_structures/03_solution/queue_solution/src/queue.py
@@ -1,6 +1,15 @@
 class Queue:
-    def __init__(self, max_size):
+    def __init__(self, max_size=5):
         self.storage = {}
         self.back = 0
         self.front = 0
         self.max_size = max_size
+
+    def enqueue(self, item):
+        if len(self.storage) < self.max_size:
+            self.storage[self.back] = item
+            self.back += 1
+
+
+# A dequeue method that removes items from the front of the queue,
+# provided the queue isn't already empty.
--- seminar_repos/02_python_fun/06_data_structures/03_solution/queue_solution/test/test_queue.py
+++ seminar_repos/02_python_fun/06_data_structures/03_solution/queue_solution/test/test_queue.py
@@ -1,7 +1,9 @@
 from src.queue import Queue
+import pytest
 
 
 class TestAttributes:
+    @pytest.mark.it("Storage should be set to default empty dictionary")
     def test_storage_attribute(self):
         test_queue = Queue(10)
         assert test_queue.storage == {}
@@ -21,3 +23,43 @@ class TestAttributes:
     def test_max_size_default_attribute(self):
         test_queue = Queue()
         assert test_queue.max_size == 5
+
+
+class TestEnqueue:
+    # Test item is added to strage dictionary
+    def test_item_is_added_to_storage(self):
+        test_queue = Queue()
+        test_queue.enqueue("apple")
+
+        assert test_queue.storage == {0: "apple"}
+
+    # Test back property in incremented
+    def test_multiple_items_added_update_back_attribute(self):
+        test_queue = Queue()
+
+        test_queue.enqueue("apple")
+        assert test_queue.back == 1
+
+        test_queue.enqueue("orange")
+        assert test_queue.back == 2
+
+        assert test_queue.storage == {0: "apple", 1: "orange"}
+
+    # Test that no more items added if queue is full
+    def test_no_more_items_added_when_queue_is_full(self):
+        test_queue = Queue(1)
+
+        test_queue.enqueue("apple")
+        test_queue.enqueue("orange")
+
+        assert test_queue.back == 1
+        assert test_queue.storage == {0: "apple"}
+
+
+class TestDequeue:
+    pass
+
+
+# remove the first item from storage
+# increment the front attribute
+# if storage is empty - no action taken
