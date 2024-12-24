305f872d00cd25c9c92fdbb4a9bcb9c8a9bb991b
nc-danika-crawley <danika.crawley@northcoders.com>
2024-12-24 11:30:01 +0000

    BACKUP_2024-12-24

--- seminar_repos/02_python_fun/07_pokemon/error_handling_and_tdd/src/user_validator.py
+++ seminar_repos/02_python_fun/07_pokemon/error_handling_and_tdd/src/user_validator.py
@@ -1,11 +1,46 @@
+class ValidationError(Exception):
+    pass
+
+
 class UserValidator:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.is_valid = False
+        self.is_valid = None
 
     def validate(self):
-        pass
+        if type(self.name) is not str:
+            raise TypeError("Name must be a string")
+        if len(self.name) < 1:
+            raise ValidationError("Name must have a length of 1 or greater")
 
     def set_is_valid(self):
-        pass
+        try:
+            self.validate()
+            self.is_valid = True
+        except Exception as e:
+            print(e)
+            self.is_valid = False
+
+
+if __name__ == "__main__":
+    user = UserValidator("", 57)
+    user.set_is_valid()
+    print(user.is_valid)
+
+
+# Create a class: UserValidator
+# UserValidator should be instantiated with a
+# name and an age.
+
+# The validate method should check the given
+# name and age meets the requirements:
+#  - name should be a string with a length > 0
+#  - age should be a positive integer no larger than 120
+
+# If the requirements aren’t satisfied then validate
+#  should raise an appropriate Error
+
+# The set_is_valid method should set the the
+# is_valid attribute to a booelan value representing
+#  the user’s validity.
--- seminar_repos/02_python_fun/07_pokemon/error_handling_and_tdd/test/test_user_validator.py
+++ seminar_repos/02_python_fun/07_pokemon/error_handling_and_tdd/test/test_user_validator.py
@@ -1,4 +1,5 @@
-from src.user_validator_end import UserValidator
+from src.user_validator import UserValidator, ValidationError
+import pytest
 
 
 class TestUserValidatorAttributes:
@@ -7,3 +8,43 @@ class TestUserValidatorAttributes:
         assert user.name == "Geoff"
         assert user.age == 57
         assert not user.is_valid
+
+
+class TestUserValidatorValidateMethod:
+    # test if name is not a string: raise exception
+    def test_validate_name_not_string(self):
+        user = UserValidator(False, 57)
+
+        with pytest.raises(TypeError, match="Name must be a string"):
+            user.validate()
+
+    # test if length of name < 1: raise exception
+    def test_validate_invalid_string_length(self):
+        user = UserValidator("", 57)
+
+        with pytest.raises(
+            ValidationError, match="Name must have a length of 1 or greater"
+        ):
+            user.validate()
+
+
+# test if age is not integer: raise exception
+# if int is not in accepted range: raise exception
+
+# if user is valid: no exception raised
+
+
+class TestUserValidatorSetIsValid:
+    def test_sets_is_valid_to_true_if_user_data_is_valid(self):
+        user = UserValidator("Jenny", 57)
+
+        user.set_is_valid()
+
+        assert user.is_valid is True
+
+    def test_set_is_valid_to_false_if_user_data_is_not_valid(self):
+        user = UserValidator(False, 57)
+
+        user.set_is_valid()
+
+        assert user.is_valid is False
